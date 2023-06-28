e3815fef859fd8fbb9734ffe16abd1c3df01d5c8
Dallen Hatton <Dhatton2001@gmail.com>
2023-06-28 19:12:26 -0400

    clean up

--- src/data.js
+++ src/data.js
@@ -1,51 +1,19 @@
-import CodingQuiz from './assets/images/Coding-Quiz-Challenge.png';
-import Giffy from './assets/images/In-A-GIFFY.png';
-import PasswordGenerator from './assets/images/Passworde-Generator.png';
-import TechTurbo from './assets/images/Tech-Turbo-Blogsite.png';
-import WeatherApp from './assets/images/Whether-The-Weather.png';
-import WorkScheduler from './assets/images/Work-Day-Scheduler.png';
+import CodingQuiz from './assets/images/Portfolio.png';
+import Giffy from './assets/images/Game Finder.png';
 
-/*export default [
+export default [
     {
         "id": 1,
-        "name": "In A GIFFY",
-        "image": Giffy,
-        "github": "https://github.com/nKingchaos2284/bananas-pajamas",
-        "deploy": "https://in-a-gif.herokuapp.com/"
+        "name": "Portfolio",
+        "image": Portfolio,
+        "github": "https://github.com/Dhatton01/Portfolio-redo",
+        "deploy": "https://dhatton01.github.io/Portfolio-redo/"
     },
     {
         "id": 2,
-        "name": "Whether The Weather",
-        "image": WeatherApp,
-        "github": "https://github.com/KyleKryst/Whether-The-Weather",
-        "deploy": "https://kylekryst.github.io/Whether-The-Weather/"
-    },
-    {
-        "id": 3,
-        "name": "Tech Turbo Blogsite",
-        "image": TechTurbo,
-        "github": "https://github.com/KyleKryst/tech-turbo-blogsite",
-        "deploy": "https://tech-turbo-blogsite.herokuapp.com/"
-    },
-    {
-        "id": 4,
-        "name": "Password Generator",
-        "image": PasswordGenerator,
-        "github": "https://github.com/KyleKryst/Password-Generator",
-        "deploy": "https://kylekryst.github.io/Password-Generator/"
-    },
-    {
-        "id": 5,
-        "name": "Work Day Scheduler",
-        "image": WorkScheduler,
-        "github": "https://github.com/KyleKryst/Work-Day-Scheduler",
-        "deploy": "https://kylekryst.github.io/Work-Day-Scheduler/"
-    },
-    {
-        "id": 6,
-        "name": "Coding Quiz Challenge",
-        "image": CodingQuiz,
-        "github": "https://github.com/KyleKryst/Coding-Quiz-Challenge",
-        "deploy": "https://kylekryst.github.io/Coding-Quiz-Challenge/"
+        "name": "Game Finder",
+        "image": Game Finder,
+        "github": "https://github.com/josephash/Game-Finder",
+        "deploy": "https://josephash.github.io/Game-Finder/"
     },
 ]
